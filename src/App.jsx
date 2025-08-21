4e32351f437d302d5edb674de71d49b9f291b708
Vaishu0805 <chilakanagavaishnavi@gmail.com>
2025-08-21 11:53:32 +0530

    Fixed files in source folder

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,17 @@ import './App.css'
 import Login from './components/Login';
 import Signup from './components/Signup';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from './components/HomePage';
-import AboutUs from './components/AboutUs';
-import Computers from './components/Computers';
-import Mobiles from './components/Mobiles';
-import Laptops from './components/Laptops';
-import Pendrives from './components/Pendrives';
-import Home from './components/Home'
-import ProductPage from './components/ProductPage'
-import Cart from './components/Cart';
-import Payment from './components/Payment';
-import Orders from './components/Orders';
+import HomePage from './HomePage';
+import AboutUs from './AboutUs';
+import Computers from './Computers';
+import Mobiles from './Mobiles';
+import Laptops from './Laptops';
+import Pendrives from './Pendrives';
+import Home from './Home'
+import ProductPage from './ProductPage'
+import Cart from './Cart';
+import Payment from './Payment';
+import Orders from './Orders';
 
 
 
